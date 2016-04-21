3205f175e860b6f28250d6af586d7f4beedc8d69
Alexander Ellis <ellisonjtk@gmail.com>
2016-04-21 18:04:00 -0400

    timestamp change

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var http = require('http');
 
-console.log("hey");
+var server = http.createServer(function(req, res) {
+  var string = req.url;
+  string = string.replace(/%20/g, ' ');
+  
+  console.log(string);
+  
+  res.end(string);
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(){
+  console.log("Server listening at:" + port);
+});
