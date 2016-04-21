3624d9ce14c48fc5eafca4e406a7dcc3a7c40324
Alexander Ellis <ellisonjtk@gmail.com>
2016-04-21 18:27:03 -0400

    server correctly set up now

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   var string = req.url;
+  string = string.slice(1,string.length);
   string = string.replace(/%20/g, ' ');
   
   console.log(string);
-  
-  res.end(string);
+  if (string.length === 0) {
+  	res.write("<h1>Please enter the date you want to check following the '/' in the url.</h1><br><h2>Example: https://enigmatic-chamber-67781.herokuapp.com/December%2012,%202012");
+  	res.end();
+  }
+  else {
+  	res.end(string);
+  }
 });
 
 var port = process.env.PORT || 3000;
